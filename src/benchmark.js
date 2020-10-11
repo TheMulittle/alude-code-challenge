86b150e6e2951d957bc16f933661251ec1153970
Murilo Kramar <murilogabardo@gmail.com>
2020-10-11 13:15:13 -0300

    Refatora a arquivo com benchmark

--- src/benchmark.js
+++ src/benchmark.js
@@ -3,42 +3,53 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 let Benchmarkify = require("benchmarkify");
 
-const emptyStack = new AludeStack();
-const numValuesEmptyStack = 10;
-ammendStack(emptyStack, numValuesEmptyStack);
+function main() {
+  const emptyStack = new AludeStack();
+  const numValuesEmptyStack = 10;
+  ammendStack(emptyStack, numValuesEmptyStack);
 
-const fullStack = new AludeStack();
-const numValuesFullStack = 100000;
-ammendStack(fullStack, numValuesFullStack);
+  const fullStack = new AludeStack();
+  const numValuesFullStack = 100000;
+  ammendStack(fullStack, numValuesFullStack);
 
-let benchmark = new Benchmarkify("AludeStack benchmark").printHeader();
+  let benchmark = new Benchmarkify("AludeStack benchmark").printHeader();
 
-let pushSuite = createBenchMarkSuite(benchmark, "Push");
+  let pushSuite = createBenchMarkSuite(
+    benchmark,
+    "Push",
+    emptyStack,
+    fullStack
+  );
 
-let peekMinSuite = createBenchMarkSuite(benchmark, "PeekMin");
+  let peekMinSuite = createBenchMarkSuite(
+    benchmark,
+    "PeekMin",
+    emptyStack,
+    fullStack
+  );
 
-let popSuite = createBenchMarkSuite(benchmark, "Pop");
+  let popSuite = createBenchMarkSuite(
+    benchmark, 
+    "Pop", 
+    emptyStack, 
+    fullStack
+  );
 
-benchmark.run([pushSuite, peekMinSuite, popSuite]);
+  benchmark.run([pushSuite, peekMinSuite, popSuite]);
+}
 
-function createBenchMarkSuite(benchmark, operation) {
+function createBenchMarkSuite(benchmark, operation, emptyStack, fullStack) {
   const suite = benchmark.createSuite(`Benchmark Operacao ${operation}`, {
     minSamples: 20,
   });
 
-  suite.ref(
-    `${operation} em AludeStack com ${numValuesEmptyStack} itens`,
-    () => {
-      emptyStack.pop();
-    }
-  );
+  suite.ref(`${operation} em AludeStack com ${emptyStack.size()} itens`, () => {
+    emptyStack.pop();
+  });
 
-  suite.add(
-    `${operation} em AludeStack com ${numValuesFullStack} itens`,
-    () => {
-      fullStack.pop();
-    }
-  );
+  suite.add(`${operation} em AludeStack com ${fullStack.size()} itens`, () => {
+    fullStack.pop();
+  });
 
   return suite;
 }
@@ -49,3 +60,5 @@ function ammendStack(stack, size) {
     stack.push(randomFloat);
   }
 }
+
+export default main();
